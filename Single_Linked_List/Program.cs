fb03aceee2dc1c5e41df381904ce4a1b7392058d
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-11-07 01:58:05 -0800

    add a node in the list

--- Single_Linked_List/Program.cs
+++ Single_Linked_List/Program.cs
@@ -13,7 +13,24 @@ namespace Single_Linked_List
 
     class List
     {
+        Node START;
+        public List()
+        {
+            START = null;
+        }
+        public void addNote() //add a node in the list
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll name of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
 
+        }
     }
 
     class Program
