a54a149540d78e4f63cf9b4fc433bb4d18dc0c5e
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-11-07 01:48:33 -0800

    Add project files.

---
+++ Single_Linked_List/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Single_Linked_List
+{
+    // each node consist of the information part and link to the next node
+    class Node
+    {
+        public int rollNumber;
+        public string name;
+        public Node next;
+
+    }
+
+    class List
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
